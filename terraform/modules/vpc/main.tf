30eda1bdcc0168c4a962531c47058dbb9ead2ef5
Shashanksingh15101998 <shashanksingh179.ss@gmail.com>
2026-01-08 19:11:03 +0530

    Create main.tf

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  project                 = var.project
+  auto_create_subnetworks = false
+}
+
+
+resource "google_compute_subnetwork" "private_subnet" {
+  name                     = "${var.vpc_name}-subnet"
+  region                   = var.region
+  network                  = google_compute_network.vpc.id
+  ip_cidr_range            = var.subnet_cidr
+  private_ip_google_access = true  # Required for Cloud NAT & Google APIs
+}
+ 
