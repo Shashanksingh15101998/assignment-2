946f0d075e1340566418c2951c496d441633ecd0
Shashanksingh15101998 <shashanksingh179.ss@gmail.com>
2026-01-08 19:00:54 +0530

    Create main.tf

---
+++ terraform/modules/firewall/main.tf
@@ -0,0 +1,20 @@
+resource "google_compute_firewall" "allow_lb_http" {
+  name    = var.firewall_name
+  network = var.network_id
+
+  direction = "INGRESS"
+
+  # Google Load Balancer IP ranges ONLY
+  source_ranges = [
+    "130.211.0.0/22",
+    "35.191.0.0/16" 
+  ]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  # Apply only to web VM
+  target_tags = ["web-server"]
+}
