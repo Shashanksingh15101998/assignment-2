0665da5e07452075d6c3fdbdeee79c381f67d276
Shashanksingh15101998 <shashanksingh179.ss@gmail.com>
2026-01-08 19:11:31 +0530

    Create outputs.tf

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,10 @@
+output "network_id" {
+  value       = google_compute_network.vpc.id
+  description = "VPC network id"
+}
+
+
+output "subnet_id" {
+  value       = google_compute_subnetwork.private_subnet.id
+  description = "Subnet id"
+}
